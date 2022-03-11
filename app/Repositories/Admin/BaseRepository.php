f0faba5c4624c851af1185d6aabc3056f7f2b4e4
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-03-11 09:05:28 +0700

    task: create admin repository

---
+++ app/Repositories/Admin/BaseRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repositories\Admin;
+
+abstract class BaseRepository implements BaseRepositoryInterface
+{
+    protected $model;
+
+    public function __construct()
+    {
+        $this->setModel();
+    }
+
+    abstract public function getModel();
+
+    public function setModel()
+    {
+        $this->model = app()->make($this->getModel());
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $attributes)
+    {
+        return $this->model->create($attributes);
+    }
+
+    public function update(int $id, array $attributes)
+    {
+        $result = $this->model->findOrFail($id);
+
+        $result->update($attributes);
+
+        return $result;
+    }
+
+    public function delete(int $id)
+    {
+        $result = $this->model->findOrFail($id);
+
+        return $result->delete();
+    }
+}
