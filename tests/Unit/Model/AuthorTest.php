d63a586f50ff0556a750c61623df09b0ff695885
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-03-24 14:56:08 +0700

    test: author model

---
+++ tests/Unit/Model/AuthorTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit\Model;
+
+use App\Models\Author;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Tests\TestCase;
+
+class AuthorTest extends TestCase
+{
+    protected $author;
+
+    /**
+     * This method is called before each test.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->author = new Author();
+    }
+
+    /**
+     * This method is called after each test.
+     */
+    public function tearDown(): void
+    {
+        unset($this->author);
+        parent::tearDown();
+    }
+
+    /**
+     * This method test $fillable property
+     */
+    public function testValidFillableProperties()
+    {
+        $fillable = [
+            'name',
+            'thumbnail',
+            'description',
+        ];
+
+        $this->assertEquals($fillable, $this->author->getFillable());
+    }
+
+    /**
+     * This method test primary key
+     */
+    public function testPrimaryKey()
+    {
+        $this->assertEquals('id', $this->author->getKeyName());
+    }
+
+    /**
+     * This method test relationship with album model
+     */
+    public function testAlbumRelation()
+    {
+        $relation = $this->author->albums();
+
+        $this->assertInstanceOf(HasMany::class, $relation);
+        $this->assertEquals('author_id', $relation->getForeignKeyName());
+        $this->assertEquals('id', $relation->getLocalKeyName());
+    }
+
+    /**
+     * This method test relationship with song model
+     */
+    public function testSongRelation()
+    {
+        $relation = $this->author->songs();
+
+        $this->assertInstanceOf(BelongsToMany::class, $relation);
+        $this->assertEquals('author_song', $relation->getTable());
+        $this->assertEquals('author_song.author_id', $relation->getQualifiedForeignPivotKeyName());
+        $this->assertEquals('author_song.song_id', $relation->getQualifiedRelatedPivotKeyName());
+    }
+}
